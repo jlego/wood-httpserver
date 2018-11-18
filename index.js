ca9ccdd54cfc2d30356ec4e02c16f7a78762e0ab
jlego <273734893@qq.com>
2018-11-19 02:06:54 +0800

    no message

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
  * http服务
  * by jlego on 2018-11-18
  */
+const cluster = require('cluster');
+const http = require('http');
+const https = require('https');
 
 module.exports = (app, config = {}) => {
   // 返回错误信息
@@ -16,34 +19,41 @@ module.exports = (app, config = {}) => {
       }
       return;
     }
-    // else{
-    //   res.status(404);
-    //   res.print(app.error_code.error_noroute);
-    // }
   });
 
-  // 监听服务端口
-  if(config.http){
-    const http = require('http');
-    const httpServer = http.createServer(app.application).listen(
-      config.http.port,
-      () => {
-        let host = httpServer.address().address;
-        let port = httpServer.address().port;
-        console.log('http server running at http://' + host + ':' + port);
-      }
-    );
-  }
-  if(config.https){
-   const https = require('https');
-    const httpsServer = https.createServer(config.https.options || {}, app.application).listen(
-      config.https.port,
-      () => {
-        let host = httpsServer.address().address;
-        let port = httpsServer.address().port;
-        console.log('https server running at http://' + host + ':' + port);
-      }
-    );
+  const cpuNums = app.config.cluster.cpus <= 0 ? require('os').cpus().length : app.config.cluster.cpus;
+  if (cluster.isMaster) {
+    for (let i = 0; i < cpuNums; i++) {
+      cluster.fork();
+    }
+    cluster.on('disconnect', (worker) => {
+      console.log(`The worker #${worker.id} has disconnected`);
+      worker.kill();
+    });
+    cluster.on('exit', (worker, code, signal) => {
+      console.log('worker %d died (%s). restarting...', worker.process.pid, signal || code);
+      cluster.fork();
+    });
+  } else {
+    console.log('The slaver[' + cluster.worker.id + ']');
+    if(config.http){
+      const httpServer = http.createServer(app.application)
+        .listen(config.http.port, () => {
+          let host = httpServer.address().address;
+          let port = httpServer.address().port;
+          console.log('http server running at http://' + host + ':' + port);
+        }
+      );
+    }
+    if(config.https){
+      const httpsServer = https.createServer(config.https.options || {}, app.application)
+        .listen(config.https.port, () => {
+          let host = httpsServer.address().address;
+          let port = httpsServer.address().port;
+          console.log('https server running at http://' + host + ':' + port);
+        }
+      );
+    }
   }
   return app;
 }
